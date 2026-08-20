d725498ce5c45e6a5effdf2edad79c628eea5917
selfwilled6412-cyber <selfwilled6412@gmail.com>
2026-08-21 02:22:18 +0900

    Load automatic deliverables styles

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import "./premium-play.css";
 import "./premium-play-finish.css";
 import "./premium-secondary.css";
 import "./premium-progress.css";
+import "./auto-deliverables.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
